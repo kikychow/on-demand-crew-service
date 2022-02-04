6a952f9f3036034c01dc82d5b1719128e1b31302
kikychow <chowkiky@gmail.com>
2022-02-04 21:46:30 +0000

    Add flight filtering for availability service

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const fs = require("fs");
 
@@ -9,18 +10,18 @@ const app = express();
 app.get("/pilots/availability", (req, res) => {
   const filters = req.query;
   console.log(filters);
-  const depDate = new Date(filters["depDateTime"]);
-  const retDate = new Date(filters["returnDateTime"]);
-  if (depDate > retDate) {
+  const filterDepDate = new Date(filters["depDateTime"]);
+  const filterRetDate = new Date(filters["returnDateTime"]);
+  if (filterDepDate > filterRetDate) {
     return res.status(400).send({
       message: "Departure Date cannot be later than Return Date",
     });
   }
-  const days = getDays(depDate, retDate);
-  fs.readFile("availability.json", function (err, data) {
-    data = JSON.parse(data);
+  const days = getDays(filterDepDate, filterRetDate);
+  fs.readFile("availability.json", function (err, data1) {
+    data1 = JSON.parse(data1);
 
-    const filteredPilots = data["Crew"].filter((pilot) => {
+    const filteredPilots = data1["Crew"].filter((pilot) => {
       let isValid = true;
 
       // Check location / Base
@@ -29,6 +30,21 @@ app.get("/pilots/availability", (req, res) => {
       // Check WorkDays
       days.forEach((day) => {
         isValid = isValid && pilot["WorkDays"].includes(day);
+      });
+
+      //Check if pilot is already scheduled to fly
+      const jsonData = fs.readFileSync("flights.json");
+      data2 = JSON.parse(jsonData);
+      const scheduledFlights = data2["Flight"].filter(
+        (p) => p["pilotId"] === pilot["ID"]
+      );
+      scheduledFlights.forEach((flight) => {
+        const flightDepDate = new Date(flight["depDateTime"]);
+        const flightRetDate = new Date(flight["returnDateTime"]);
+        console.log(flightDepDate, filterRetDate, flightRetDate, filterDepDate);
+        const isOverlap =
+          flightDepDate <= filterRetDate && flightRetDate >= filterDepDate;
+        isValid = isValid && !isOverlap;
       });
 
       return isValid;
@@ -50,13 +66,12 @@ function getDays(depDate, retDate) {
   ];
   let res = [];
   const daysInAWeek = 7;
-  const curDate = depDate;
+  const curDate = new Date(depDate);
   while (curDate <= retDate && res.length < daysInAWeek) {
     const dayOfWeek = curDate.getDay();
     res.push(dayNames[dayOfWeek]);
     curDate.setDate(curDate.getDate() + 1);
   }
-  console.log(res);
   return res;
 }
 
