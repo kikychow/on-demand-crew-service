723431822959ee4bd04f86812cdc64f6bf9be9d6
kikychow <chowkiky@gmail.com>
2022-02-05 00:42:32 +0000

    Add availability check in service to schedule a flight for a pilot

--- routes/flights.js
+++ routes/flights.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const flightsRoutes = express.Router();
 const fs = require("fs");
+const request = require("request");
 
 const dataPath = "./database/flights.json";
 
@@ -14,13 +16,42 @@ const getFlightsData = () => {
   return JSON.parse(jsonData);
 };
 
+// Schedule a Flight for a Pilot
+// Example POST
+// localhost/flights {"pilotId": 1823, "depDateTime": "2025-08-01T09:00:00Z", "returnDateTime": "2025-08-01T10:00:00Z"}
 flightsRoutes.post("/", (req, res) => {
-  const newFlight = req.body
+  const newFlight = req.body;
+  console.log(newFlight);
   let existFlights = getFlightsData();
-  existFlights["Flight"].push(newFlight)
 
-  saveFlightsData(existFlights);
-  res.status(201).json(newFlight);
+  // Check whether pilot's slot is available
+  const port = process.env.PORT || 3000;
+  const host = "http://localhost:" + port;
+  const url =
+    host +
+    "/pilots/availability?depDateTime=" +
+    newFlight["depDateTime"] +
+    "&returnDateTime=" +
+    newFlight["returnDateTime"] +
+    "&ID=" +
+    newFlight["pilotId"];
+  console.log(url);
+  request(url, function (error, response, body) {
+    pilotsData = JSON.parse(body);
+    console.log(pilotsData);
+    const isAvailable = pilotsData.length > 0;
+
+    if (isAvailable) {
+      existFlights["Flight"].push(newFlight);
+
+      saveFlightsData(existFlights);
+      res.status(201).json(newFlight);
+    } else {
+      res.status(400).send({
+        message: "Pilot's slot is not available",
+      });
+    }
+  });
 });
 
 module.exports = flightsRoutes;
--- routes/pilots.js
+++ routes/pilots.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 
 const dataPath = "./database/pilots.json";
 
-const savePilotsData = (data) => {
-  const stringifyData = JSON.stringify(data);
-  fs.writeFileSync(dataPath, stringifyData);
-};
+// const savePilotsData = (data) => {
+//   const stringifyData = JSON.stringify(data);
+//   fs.writeFileSync(dataPath, stringifyData);
+// };
 
 const getPilotsData = () => {
   const jsonData = fs.readFileSync(dataPath);
@@ -19,43 +19,67 @@ const getPilotsData = () => {
 // localhost/pilots/availability?location=Munich&depDateTime=2025-08-01T00:00:00Z&returnDateTime=2025-08-02T00:00:00Z
 pilotsRoutes.get("/availability", (req, res) => {
   const filters = req.query;
-  console.log(filters);
-  const filterDepDate = new Date(filters["depDateTime"]);
-  const filterRetDate = new Date(filters["returnDateTime"]);
-  if (filterDepDate > filterRetDate) {
-    return res.status(400).send({
-      message: "Departure Date cannot be later than Return Date",
-    });
+  let hasLocationFilter = filters["location"] !== undefined;
+  let hasDateTimeFilter =
+    filters["depDateTime"] !== undefined &&
+    filters["returnDateTime"] !== undefined;
+  let hasPilotIdFilter = filters["ID"] !== undefined;
+
+  let days = [];
+  let filterDepDate;
+  let filterRetDate;
+  if (hasDateTimeFilter) {
+    filterDepDate = new Date(filters["depDateTime"]);
+    filterRetDate = new Date(filters["returnDateTime"]);
+    if (filterDepDate > filterRetDate) {
+      return res.status(400).send({
+        message: "Departure Date cannot be later than Return Date",
+      });
+    }
+    days = getDays(filterDepDate, filterRetDate);
   }
-  const days = getDays(filterDepDate, filterRetDate);
+
   const pilotsData = getPilotsData();
 
   const filteredPilots = pilotsData["Crew"].filter((pilot) => {
     let isValid = true;
 
+    // Check pilot id
+    if (hasPilotIdFilter) {
+        if (pilot["ID"] !== parseInt(filters["ID"])) {
+            return false
+        }
+    }
+
     // Check location / Base
-    isValid = isValid && pilot["Base"] == filters["location"];
+    if (hasLocationFilter) {
+      isValid = isValid && pilot["Base"] == filters["location"];
+    }
 
     // Check WorkDays
-    days.forEach((day) => {
-      isValid = isValid && pilot["WorkDays"].includes(day);
-    });
+    if (hasDateTimeFilter) {
+      days.forEach((day) => {
+        isValid = isValid && pilot["WorkDays"].includes(day);
+      });
+    }
 
     //Check if pilot is already scheduled to fly
-    const flightDataPath = "./database/flights.json";
-    const jsonData = fs.readFileSync(flightDataPath);
-    const flightsData = JSON.parse(jsonData);
-    const scheduledFlights = flightsData["Flight"].filter(
-      (p) => p["pilotId"] === pilot["ID"]
-    );
-    scheduledFlights.forEach((flight) => {
-      const flightDepDate = new Date(flight["depDateTime"]);
-      const flightRetDate = new Date(flight["returnDateTime"]);
-      console.log(flightDepDate, filterRetDate, flightRetDate, filterDepDate);
-      const isOverlap =
-        flightDepDate <= filterRetDate && flightRetDate >= filterDepDate;
-      isValid = isValid && !isOverlap;
-    });
+    if (hasDateTimeFilter) {
+      const flightDataPath = "./database/flights.json";
+      const jsonData = fs.readFileSync(flightDataPath);
+      const flightsData = JSON.parse(jsonData);
+      const scheduledFlights = flightsData["Flight"].filter(
+        (p) => p["pilotId"] === pilot["ID"]
+      );
+      scheduledFlights.forEach((flight) => {
+        const flightDepDate = new Date(flight["depDateTime"]);
+        const flightRetDate = new Date(flight["returnDateTime"]);
+        console.log(flightDepDate, filterRetDate, flightRetDate, filterDepDate);
+        const isOverlap =
+          flightDepDate <= filterRetDate && flightRetDate >= filterDepDate;
+        isValid = isValid && !isOverlap;
+      });
+    }
 
     return isValid;
   });
